97ba0a6f6bc0e6d6e0419b903d87cabe21c02afc
Gleb <prasionissy@gmail.com>
2024-05-24 10:12:57 +0500

    booking view added

--- src/views/AdminBookingView.vue
+++ src/views/AdminBookingView.vue
@@ -1,19 +1,56 @@
 <template>
-  <h1>ADMIN BOOKING</h1>
+  <div class="admin-booking-view">
+    <table-item
+    :objectsList="optionsList"
+    >
+
+    </table-item>
+  </div>
 </template>
 
 <script>
+  import TableItem from "@/components/UI/TableItem.vue";
+
   export default {
     data() {
       return {
-        
+        components: {
+          TableItem
+        },
+        optionsList: [
+          {
+            'ID места': 1,
+            'Дата брони': '22.22.2022',
+            'Промежуток времени': '16:00-18:00'
+          },
+          {
+            'ID места': 1,
+            'Дата брони': '22.22.2022',
+            'Промежуток времени': '16:00-18:00'
+          },
+          {
+            'ID места': 1,
+            'Дата брони': '22.22.2022',
+            'Промежуток времени': '16:00-18:00'
+          },
+          {
+            'ID места': 1,
+            'Дата брони': '22.22.2022',
+            'Промежуток времени': '16:00-18:00'
+          },
+          {
+            'ID места': 1,
+            'Дата брони': '22.22.2022',
+            'Промежуток времени': '16:00-18:00'
+          }
+        ]
       }
     }
   }
 </script>
 
 <style scoped>
-  h1 {
+  .admin-booking-view {
     width: 100%;
   }
 </style>
